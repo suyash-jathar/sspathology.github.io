65ae598cc73a60b2310e8254da512bb2a93369a3
suyash-jathar <suyashjathar15@gmail.com>
2026-01-10 12:08:02 +0530

    optimizing

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "bd9bd28d5e4d3c30189604038eda68fa",
+const RESOURCES = {"flutter_bootstrap.js": "6a35fdfcbbc3d69572773267773f1aec",
 "version.json": "732ddb0ad9aeb6a2632585c05985f30c",
 "index.html": "b7b80e5c44cf4351469b50e050f13c12",
 "/": "b7b80e5c44cf4351469b50e050f13c12",
-"main.dart.js": "6bf76f2e59bae9a7e6d0f482216a9f2c",
+"main.dart.js": "c2d67bec058643717d1f31249e7a0358",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
