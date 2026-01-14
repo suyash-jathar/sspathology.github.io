6d08e32bb2db473f163ec47bffb36490c0a67b02
suyash-jathar <suyashjathar15@gmail.com>
2026-01-14 16:52:39 +0530

    Optimizing_Website_New_UI

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4229027122"
+    serviceWorkerVersion: "1795993740"
   }
 });
