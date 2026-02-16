d811931b776e28f0d0f7d509a66105a2b6d71712
suyash-jathar <suyashjathar15@gmail.com>
2026-02-16 21:00:01 +0530

    Optimizng Website for View PDF

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "bc847714287c6e62191557a1dfed69aa",
+const RESOURCES = {"flutter_bootstrap.js": "a0658d86fc70f76e87661d72723478aa",
 "version.json": "732ddb0ad9aeb6a2632585c05985f30c",
 "index.html": "b7b80e5c44cf4351469b50e050f13c12",
 "/": "b7b80e5c44cf4351469b50e050f13c12",
-"main.dart.js": "2eeb6c6f3131a37e9cc9df120cd8278b",
+"main.dart.js": "69cb974cf0fabb75fe8331e865934a50",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
