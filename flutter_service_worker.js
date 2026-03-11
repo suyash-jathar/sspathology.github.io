c1a0348eaee21bda02bb4f77df3cf58417151a26
suyash-jathar <suyashjathar15@gmail.com>
2026-03-12 01:09:12 +0530

    trying to fix the live update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,10 +3,10 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "11a0587407ded3e0e2919db9001543a7",
+const RESOURCES = {"flutter_bootstrap.js": "b2f0a33564f392cdef907c5d02c11e22",
 "version.json": "732ddb0ad9aeb6a2632585c05985f30c",
-"index.html": "b7b80e5c44cf4351469b50e050f13c12",
-"/": "b7b80e5c44cf4351469b50e050f13c12",
+"index.html": "d9058e0be40b65d4a8486a782264555c",
+"/": "d9058e0be40b65d4a8486a782264555c",
 "main.dart.js": "a9a90f8f8a2c63f2bf243080c0420a23",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
