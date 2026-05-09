412405d504d6f0b3c486e120f720871e96f3aa71
suyash-jathar <suyashjathar15@gmail.com>
2026-05-10 02:00:06 +0530

    Updating website

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4114742260" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "784868760" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
